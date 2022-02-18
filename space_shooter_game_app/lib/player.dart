712d3036485d534c90d41a96683a2b0460d90176
jdavifranco <developerjdavi@gmail.com>
2022-02-17 22:10:14 -0300

    add player model

---
+++ space_shooter_game_app/lib/player.dart
@@ -0,0 +1,17 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class Player extends SpriteComponent {
+
+  Player(
+    Sprite? sprite,
+    Vector2? position,
+    Vector2? size,
+  ) : super(
+    sprite: sprite,
+    position: position,
+    size: size,
+  );
+
+
+}
